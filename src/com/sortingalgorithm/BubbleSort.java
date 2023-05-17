0e9249d8be0bdceb4744afbb56be6f67cf4aa7f1
prithuk <prithukathet@gmail.com>
2023-05-17 15:39:46 +0545

    Bubble Sort in two ways

--- src/com/sortingalgorithm/BubbleSort.java
+++ src/com/sortingalgorithm/BubbleSort.java
@@ -1,2 +1,37 @@
-package com.sortingalgorithm;public class BubbleSort {
+package com.sortingalgorithm;
+
+public class BubbleSort {
+    public static void main(String[] args) {
+
+        int arr[] = {7, 8, 3, 1, 2};
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = 0; j < arr.length - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+        printArray(arr);
+
+    }
+
+    public static void printArray(int array[]) {
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + " ");
+        }
+    }
+
+//    public static void swap(int[] array, int i, int j) {
+//
+//        int temp;
+//        if (i == j) {
+//            return;
+//        }
+//        temp = array[i];
+//        array[i] = array[j];
+//        array[j] = temp;
+//
+//    }
 }
--- src/com/sortingalgorithm/SortingBubble.java
+++ src/com/sortingalgorithm/SortingBubble.java
@@ -1,2 +1,23 @@
-package com.sortingalgorithm;public class SortingBubble {
+package com.sortingalgorithm;
+
+public class SortingBubble {
+    public static void main(String[] args) {
+        int[] arrayToSort = {20, 35, -15, 7, 55, 1, -22};
+
+
+        //bubble sort
+        for(int i=0; i<arrayToSort.length; i++){
+            for(int j=1; j< arrayToSort.length-i; j++){
+                if(arrayToSort[j-1]>arrayToSort[j]){
+                    int temp = arrayToSort[j-1];
+                    arrayToSort[j-1]=arrayToSort[j];
+                    arrayToSort[j]= temp ;
+                }
+            }
+        }
+
+        for(int i=0 ; i<arrayToSort.length; i++){
+            System.out.print(arrayToSort[i]+" ");
+        }
+    }
 }
